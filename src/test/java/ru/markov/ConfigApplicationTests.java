3a2d27f883da67378b28769e25578ba7e4d6848d
Sergey <orm1@yandex.ru>
2025-01-05 08:21:39 +0300

    new project

---
+++ src/test/java/ru/markov/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.markov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
